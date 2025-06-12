98bc0432f965077117582068f07d61351b8538c0
Felix <code@f-x-a.de>
2025-06-12 13:33:57 +0200

    T0 implementation of tuner widget

--- app/src/main/java/fxa/apps/guitar_toolkit/MainActivity.kt
+++ app/src/main/java/fxa/apps/guitar_toolkit/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -72,6 +75,7 @@ fun GuitarToolkitApp() {
 fun AudioVisualizerWithPitch() {
     var pitch by remember { mutableFloatStateOf(-1f) }
     var level by remember { mutableFloatStateOf(0f) }
+    var selectedTargetPitch by remember { mutableFloatStateOf(0f)}
 
     val note = frequencyToNoteName(pitch)
 
@@ -113,30 +117,112 @@ fun AudioVisualizerWithPitch() {
         }
     }
 
+    val boxModifier = Modifier
+        .height(50.dp)
+        .width(25.dp)
+        .border(BorderStroke(1.dp,Color.White))
+
+    val pitchButtonModifier = Modifier
+        .width(50.dp)
+        .padding(5.dp)
+
+    var box1Color = Color.Transparent
+    var box2Color = Color.Transparent
+    var box3Color = Color.Transparent
+    var box4Color = Color.Transparent
+    var box5Color = Color.Transparent
+    var centerBoxColor = Color.Transparent
+    var box6Color = Color.Transparent
+    var box7Color = Color.Transparent
+    var box8Color = Color.Transparent
+    var box9Color = Color.Transparent
+    var box10Color = Color.Transparent
+
+    val lowerPitchTolerance = selectedTargetPitch - 3F
+    val upperPitchTolerance = selectedTargetPitch + 3F
+
+
+    //debug pitch input
+    //pitch = 440F
+
+    if(pitch in lowerPitchTolerance..upperPitchTolerance){
+        centerBoxColor = Color.Green
+    } else {
+        when {
+            pitch <= selectedTargetPitch-15F -> box1Color = Color.Red
+            pitch <= selectedTargetPitch-12F && pitch >=selectedTargetPitch-15F -> box2Color = Color.Red
+            pitch <= selectedTargetPitch-9F && pitch >=selectedTargetPitch-12F -> box3Color = Color.Yellow
+            pitch <= selectedTargetPitch-6F && pitch >=selectedTargetPitch-9F -> box4Color = Color.Yellow
+            pitch <= selectedTargetPitch-3F && pitch >=selectedTargetPitch-6F -> box5Color = Color.Yellow
+
+            pitch >= selectedTargetPitch+3F && pitch <= selectedTargetPitch+6F -> box6Color = Color.Yellow
+            pitch >= selectedTargetPitch+6F && pitch <= selectedTargetPitch+9F -> box7Color = Color.Yellow
+            pitch >= selectedTargetPitch+9F && pitch <= selectedTargetPitch+12F -> box8Color = Color.Yellow
+            pitch >= selectedTargetPitch+12F && pitch <= selectedTargetPitch+15F -> box9Color = Color.Red
+            pitch >= selectedTargetPitch+15F -> box10Color = Color.Red
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(24.dp),
         verticalArrangement = Arrangement.Bottom
     ) {
-        Text("Detected Pitch: ${if (pitch > 0) "%.2f Hz".format(pitch) else "No pitch"}")
+        /*
+        Row (Modifier.align(Alignment.CenterHorizontally)) {
+            val buttonPadding = PaddingValues(start = 0.dp, top = 0.dp, end = 0.dp, bottom = 0.dp)
+            Button(onClick = {selectedTargetPitch = 82.41F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("E")}
+            Button(onClick = {selectedTargetPitch = 110F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("A")}
+            Button(onClick = {selectedTargetPitch = 146.83F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("D")}
+            Button(onClick = {selectedTargetPitch = 196F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("G")}
+            Button(onClick = {selectedTargetPitch = 246.94F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("B")}
+            Button(onClick = {selectedTargetPitch = 329.63F},contentPadding = buttonPadding,modifier = pitchButtonModifier){Text("E")}
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text("Target pitch: $selectedTargetPitch")
+
+        */
+
+        Row (Modifier.align(Alignment.CenterHorizontally)) {
+            Box(modifier = Modifier.offset(x = -25.dp).background(box1Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = -20.dp).background(box2Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = -15.dp).background(box3Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = -10.dp).background(box4Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = -5.dp).background(box5Color).then(boxModifier))
+            //center box
+            Box(
+                modifier = Modifier.width(25.dp)
+                            .height(70.dp)
+                            .border(BorderStroke(1.dp,Color.White))
+                    .background(centerBoxColor)
+                )
+            Box(modifier = Modifier.offset(x = 5.dp).background(box6Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = 10.dp).background(box7Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = 15.dp).background(box8Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = 20.dp).background(box9Color).then(boxModifier))
+            Box(modifier = Modifier.offset(x = 25.dp).background(box10Color).then(boxModifier))
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+            Text("Detected Pitch: ${if (pitch > 0) "%.2f Hz".format(pitch) else "No pitch"}")
         Spacer(modifier = Modifier.height(8.dp))
-        Text("Note: $note", style = MaterialTheme.typography.headlineLarge)
+            Text("Note: $note", style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(16.dp))
-        Box(
-            modifier = Modifier
-                .height(20.dp)
-                .fillMaxWidth()
-                .background(Color.Gray)
-        ) {
+
+            //level meter
             Box(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(level)
-                    .background(Color.Green)
-            )
-            Text(" input level")
-        }
+                    .height(5.dp)
+                    .fillMaxWidth()
+                    .background(Color.Gray)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(level)
+                        .background(Color.Green)
+                )
+            }
     }
 }
 
